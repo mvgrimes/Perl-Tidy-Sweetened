53ab14db8268e2d5e635e5cc4f80fe0273fff514
Mark Grimes <mgrimes@cpan.org>
2016-04-15 09:31:02 -0400

    Updates version and preps for release

--- lib/Perl/Tidy/Sweetened.pm
+++ lib/Perl/Tidy/Sweetened.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 use Perl::Tidy qw();
 
-our $VERSION = '1.10';
+our $VERSION = '1.11';
 
 use Perl::Tidy::Sweetened::Pluggable;
 use Perl::Tidy::Sweetened::Keyword::Block;
@@ -88,7 +88,7 @@ Perl::Tidy::Sweetened - Tweaks to Perl::Tidy to support some syntactic sugar
 
 =head1 VERSION
 
-version 1.10
+version 1.11
 
 =head1 STATUS
 
